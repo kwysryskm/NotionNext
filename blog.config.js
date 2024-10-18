def7107513e4051ba18fb75cbf9b2780de4c0b5f
kwysryskm <162030933+kwysryskm@users.noreply.github.com>
2024-10-18 13:21:44 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -24,7 +24,7 @@ const BLOG = {
 
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
-  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || '王悦', // 您的昵称 例如 tangly1024
+  AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Erised', // 您的昵称 例如 tangly1024
   BIO: process.env.NEXT_PUBLIC_BIO || '暨南大学2023级 戏剧影视文学', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://kwysr.xyz', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客,作品集', // 网站关键词 英文逗号隔开
